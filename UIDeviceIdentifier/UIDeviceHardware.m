282e4ecc7dbd6eee50b345d440690f41641a13e5
Paul Williamson <squarefrog@gmail.com>
2015-11-25 09:51:13 +0000

    Add iPad Pro

--- UIDeviceIdentifier/UIDeviceHardware.m
+++ UIDeviceIdentifier/UIDeviceHardware.m
@@ -76,6 +76,8 @@
     if ([platform isEqualToString:@"iPad5,2"])      return @"iPad Mini 4 (WiFi/Cellular)";
     if ([platform isEqualToString:@"iPad5,3"])      return @"iPad Air 2 (WiFi)";
     if ([platform isEqualToString:@"iPad5,4"])      return @"iPad Air 2 (WiFi/Cellular)";
+    if ([platform isEqualToString:@"iPad6,7"])      return @"iPad Pro (WiFi)";
+    if ([platform isEqualToString:@"iPad6,8"])      return @"iPad Pro (WiFi/Cellular)";
     if ([platform isEqualToString:@"i386"])         return @"Simulator";
     if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
 
