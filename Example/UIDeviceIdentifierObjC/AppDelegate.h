ef89ed2ad7f5a1c54c586a4773070cfebdef0362
Antoine Cœur <coeur@gmx.fr>
2015-12-22 23:26:30 +0800

    example projects

---
+++ Example/UIDeviceIdentifierObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIDeviceIdentifier
+//
+//  Created by Antoine Cœur on 22/12/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/UIDeviceIdentifierObjC/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  UIDeviceIdentifier
+//
+//  Created by Antoine Cœur on 22/12/2015.
+//
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+@end
